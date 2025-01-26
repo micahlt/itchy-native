e72adac5755302040980927bbfb2b4194af0167c
Micah Lindley <hi@micahlindley.com>
2025-01-25 23:29:49 -0600

    Allow multiple pages of comments

--- app/user/[username]/comments.jsx
+++ app/user/[username]/comments.jsx
@@ -1,24 +1,44 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, RefreshControl } from "react-native";
 import { useTheme } from "../../../utils/theme";
 import { Stack } from "expo-router/stack";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import ScratchAPIWrapper from "../../../utils/api-wrapper";
 import Comment from "../../../components/Comment";
 
 export default function User() {
     const { username } = useLocalSearchParams();
-    const { colors } = useTheme();
+    const { colors, isDark } = useTheme();
     const [comments, setComments] = useState([]);
-    useEffect(() => {
-        ScratchAPIWrapper.user.getComments(username, 1).then((d) => {
-            setComments(d);
-        }).catch(console.error)
-    }, [username]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(true);
 
-    const renderComment = ({ item }) => {
+    useEffect(() => {
+        if (!username) return;
+        setLoading(true);
+        ScratchAPIWrapper.user.getComments(username, page).then((d) => {
+            if (page === 1) {
+                setComments(d);
+            } else {
+                setComments([...comments, ...d]);
+            }
+            setLoading(false);
+        }).catch(console.error);
+    }, [username, page]);
+
+    const renderComment = useCallback(({ item }) => {
         return <Comment comment={item} />
-    }
+    }, []);
+
+    const endReached = useCallback(() => {
+        if (loading) return;
+        setPage(page + 1);
+    }, [loading, page]);
+
+    const refresh = useCallback(() => {
+        setPage(1);
+    }, []);
+
 
     return (
         <View style={{ flex: 1, backgroundColor: colors.background }}>
@@ -28,7 +48,7 @@ export default function User() {
                 }}
             />
             {comments.length > 0 && (
-                <FlatList contentContainerStyle={{ padding: 10 }} style={{ flex: 1 }} data={comments} renderItem={renderComment} keyExtractor={(item, i) => item.id + i} />
+                <FlatList contentContainerStyle={{ padding: 10 }} style={{ flex: 1 }} data={comments} renderItem={renderComment} keyExtractor={(item, i) => item.id + i} onEndReached={endReached} onRefresh={refresh} refreshing={loading} refreshControl={<RefreshControl refreshing={loading} tintColor={"white"} progressBackgroundColor={colors.accent} colors={isDark ? ["black"] : ["white"]} />} />
             )}
         </View>
     );
--- components/Comment.jsx
+++ components/Comment.jsx
@@ -3,19 +3,22 @@ import Chip from "./Chip"
 import { decode } from "html-entities"
 import { useTheme } from "../utils/theme"
 import { useRouter } from "expo-router";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import Card from "./Card";
 
 export default function Comment({ comment, isReply = false, isLastReply = false }) {
     const { colors } = useTheme();
     const router = useRouter();
     const hasReplies = useMemo(() => comment.replies && comment.replies.length > 0, [comment]);
+    const openAuthor = useCallback(() => {
+        router.push(`/user/${comment.username}/profile`);
+    }, [comment])
 
     return (
         <View style={{ borderLeftColor: colors.backgroundTertiary, borderLeftWidth: isReply ? 3 : 0, paddingBottom: isLastReply ? 0 : 10, marginLeft: isReply ? 8 : 0 }}>
             <Card style={{ backgroundColor: colors.backgroundSecondary, padding: 15, borderRadius: 10, marginLeft: isReply ? 10 : 0, marginBottom: hasReplies ? 10 : 0 }}>
                 <View style={{ flexDirection: "row", alignItems: "top", justifyContent: "space-between" }}>
-                    <Chip.Image text={comment.username} imageURL={comment.avatarURL} mode="outlined" style={{ marginRight: "auto", marginBottom: 8 }} textStyle={{ fontWeight: "bold" }} onPress={() => router.push(`/user/${comment.username}/profile`)} />
+                    <Chip.Image text={comment.username} imageURL={comment.avatarURL} mode="outlined" style={{ marginRight: "auto", marginBottom: 8 }} textStyle={{ fontWeight: "bold" }} onPress={openAuthor} />
                     <Text style={{ color: colors.textSecondary, fontSize: 12, marginTop: 2 }}>{new Date().toLocaleString("en-US", {
                         timeStyle: "short",
                         dateStyle: "long"
