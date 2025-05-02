55247a125fad0bec30c9b2c437f02e19d6b8fda9
Micah Lindley <hi@micahlindley.com>
2025-05-02 12:14:01 -0500

    Remove opacity effects

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -46,7 +46,6 @@ export default function HomeScreen() {
     const rotate = useSharedValue(0);
     const isAtTop = useSharedValue(true);
     const didVibrate = useSharedValue(false);
-    const opacity = useAnimatedValue(1);
 
     useEffect(() => {
         refresh()
@@ -149,9 +148,9 @@ export default function HomeScreen() {
                         isAtTop.value = offsetY <= 0;
                     }}>
                     <Animated.View style={[headerStyle, { flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingTop: insets.top + 5, paddingBottom: 15, paddingHorizontal: 20, gap: 10 }]}>
-                        <Pressable onPress={() => router.push(`/users/${username}`)}><Image source={`https://uploads.scratch.mit.edu/get_image/user/${user.id}_60x60.png`} style={{ height: 35, width: 35, borderRadius: 35, opacity: opacity.value }} contentFit="stretch" /></Pressable>
+                        <Pressable onPress={() => router.push(`/users/${username}`)}><Image source={`https://uploads.scratch.mit.edu/get_image/user/${user.id}_60x60.png`} style={{ height: 35, width: 35, borderRadius: 35 }} contentFit="stretch" /></Pressable>
                         <Animated.Image source={require("../../assets/logo-nobg.png")} style={[logoStyle, { height: 50, width: 50 }]} />
-                        <Pressable onPress={() => router.push('/settings')}><MaterialIcons style={{ backgroundColor: opacity.value, marginRight: 5 }} name="settings" size={26} contentFit="cover" color={colors.textSecondary} /></Pressable>
+                        <Pressable onPress={() => router.push('/settings')}><MaterialIcons style={{ marginRight: 5 }} name="settings" size={26} contentFit="cover" color={colors.textSecondary} /></Pressable>
                     </Animated.View>
                     <Animated.View style={[contentStyle, { backgroundColor: colors.background, paddingBottom: 20, borderTopLeftRadius: 20, borderTopRightRadius: 20, paddingTop: 10 }]}>
                         {!!username ? <Feed style={{ margin: 20, marginBottom: 0, marginTop: 15 }} username={username} rerender={refreshCount} /> : <SignInPrompt />}
