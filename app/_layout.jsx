ca473f0758e65dbf382b2115494fdf8de821c5c6
Micah Lindley <hi@micahlindley.com>
2025-11-12 16:41:45 -0600

    Try bumping Reanimated version to fix crashes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -200,6 +200,7 @@ function ThemeConsumerInner({ twConfig }) {
           headerStyle: {
             backgroundColor: liquidPlus ? "transparent" : colors.background, // no fallback bg
           },
+          animationDuration: 120
         }}
       >
         <Stack.Screen
