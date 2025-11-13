654c3f878dc7a30c7d87c167c0a5819a2486475e
Micah Lindley <hi@micahlindley.com>
2025-11-12 23:20:42 -0600

    More metro fixes?

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,16 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require("path");
 const {
     wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver = {
-    ...config.resolver,
-    alias: {
-        'react': require.resolve('react'),
-        'react-native': require.resolve('react-native'),
-    },
-};
+// config.resolver = {
+//     ...config.resolver,
+//     alias: {
+//         'react': require.resolve('react'),
+//         'react-native': require.resolve('react-native'),
+//     },
+// };
 
 module.exports = wrapWithReanimatedMetroConfig(config);
