6e64e4f6eb87e6215960a8d3a703a6de0ed39ec6
Micah Lindley <hi@micahlindley.com>
2025-11-19 16:27:39 -0600

    Try to fix missing Google Service plist in CI build

--- utils/downloadGoogleServices.sh
+++ utils/downloadGoogleServices.sh
@@ -2,3 +2,5 @@
 
 echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./google-services.json
 echo $GOOGLE_SERVICES_PLIST_BASE64 | base64 --decode > ./GoogleService-Info.plist
+cp $EAS_PWD/GoogleService-info.plist $EAS_BUILD_WORKINGDIR
+echo "Copied Google Services to working directory!"
