803de7d180d7d847570e27b64ba19f9e2bcfe7a4
Micah Lindley <hi@micahlindley.com>
2025-11-19 17:12:02 -0600

    UGH

--- utils/downloadGoogleServices.sh
+++ utils/downloadGoogleServices.sh
@@ -2,7 +2,5 @@
 
 echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./google-services.json
 echo $GOOGLE_SERVICES_PLIST_BASE64 | base64 --decode > ./GoogleService-Info.plist
-cp ./GoogleService-info.plist $EAS_BUILD_WORKINGDIR/ios/Itchy/GoogleService-Info.plist
-cp ./GoogleService-info.plist $EAS_BUILD_WORKINGDIR/GoogleService-Info.plist
-echo "Copied Google Services to working directory:"
-echo $EAS_BUILD_WORKINGDIR
+#cp ./GoogleService-info.plist $EAS_BUILD_WORKINGDIR/GoogleService-Info.plist
+#echo "Copied Google Services to working directory."
