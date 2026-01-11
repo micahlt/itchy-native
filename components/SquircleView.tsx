7318a666426f8225da4e932b15423c1d02a80a08
Micah Lindley <hi@micahlindley.com>
2026-01-11 01:38:18 -0600

    Never mind again

--- components/SquircleView.tsx
+++ components/SquircleView.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Platform, StyleProp, View, ViewProps, ViewStyle } from "react-native";
-import RNSquircleView from "react-native-fast-squircle";
+import { StyleProp, View, ViewProps, ViewStyle } from "react-native";
 
 type SquircleViewProps = ViewProps & {
   style: StyleProp<ViewStyle>;
@@ -9,23 +8,11 @@ type SquircleViewProps = ViewProps & {
 
 const SquircleView = React.forwardRef<View, SquircleViewProps>(
   ({ style, children, ...props }, ref) => {
-    if (Platform.OS == "ios") {
-      return (
-        <View
-          ref={ref}
-          style={[{ borderCurve: "continuous" }, style]}
-          {...props}
-        >
-          {children}
-        </View>
-      );
-    } else {
-      return (
-        <RNSquircleView ref={ref} style={style} {...props}>
-          {children}
-        </RNSquircleView>
-      );
-    }
+    return (
+      <View ref={ref} style={[{ borderCurve: "continuous" }, style]} {...props}>
+        {children}
+      </View>
+    );
   }
 );
 
