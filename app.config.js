a365b863475da08268510270f8447802782cb474
Micah Lindley <hi@micahlindley.com>
2026-01-13 02:21:13 -0600

    Bump version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Itchy",
     slug: "itchy-native",
-    version: "2.7.3",
+    version: "2.7.4",
     scheme: "itchy",
     newArchEnabled: true,
     orientation: "portrait",
@@ -64,7 +64,7 @@ module.exports = {
     },
     userInterfaceStyle: "automatic",
     android: {
-      versionCode: 73,
+      versionCode: 74,
       package: "org.scratchclient4.itchy",
       adaptiveIcon: {
         backgroundColor: "#0082FF",
@@ -78,7 +78,7 @@ module.exports = {
     },
     ios: {
       bundleIdentifier: "org.scratchclient4.itchy",
-      buildNumber: "73",
+      buildNumber: "74",
       icon: "./assets/iosicon.icon",
       config: {
         usesNonExemptEncryption: false
