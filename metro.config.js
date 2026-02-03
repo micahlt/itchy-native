49ac22c9ea6b2e33ca7e56934b1f8c6421a119ad
Micah Lindley <hi@micahlindley.com>
2026-02-03 15:25:53 -0600

    Remove weird local development things for multiplay package

--- metro.config.js
+++ metro.config.js
@@ -14,14 +14,15 @@ const config = getDefaultConfig(__dirname);
 // };
 
 // 1. Point to your SDK folder
-const sdkRoot = path.resolve(__dirname, '../itchy-multiplay');
+// const sdkRoot = path.resolve(__dirname, '../itchy-multiplay');
 
 // 2. Add it to the folders Metro watches
-config.watchFolders = [__dirname, sdkRoot];
+// config.watchFolders = [__dirname, sdkRoot];
+config.watchFolders = [__dirname];
 
 config.resolver.nodeModulesPaths = [
     path.resolve(__dirname, 'node_modules'),
-    path.resolve(sdkRoot, 'node_modules'),
+    // path.resolve(sdkRoot, 'node_modules'),
 ];
 
 module.exports = wrapWithReanimatedMetroConfig(config);
